b95510092529286dab899bbce8cb86ab59d94b75
marpiotoun <plus470@naver.com>
2020-04-05 19:42:52 +0900

    Class Component & State
    1. shifting from function based component to Class based component
    2. npm install axios
    3. 비동기 방식 async and await

--- src/App.js
+++ src/App.js
@@ -1,56 +1,29 @@
 import React from 'react';
+import axios from "axios";
+// import Proptypes from 'prop-types';
 
 
-function Food({ name, image }) {
-  return (
-    <div>
-      <h1>I love {name} </h1>
-      <img src={image} alt={name} />
-    </div>
-  )
-}
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  };
+  getMovies = () => {
 
-const foodILike = [
-  {
-    id: 1,
-    name: "Kimchi",
-    image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
-  },
-  {
-    id: 2,
-    name: "Samgyeopsal",
-    image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg"
-  },
-  {
-    id: 3,
-    name: "Bibimbap",
-    image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
-  },
-  {
-    id: 4,
-    name: "Doncasu",
-    image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
-  },
-  {
-    id: 5,
-    name: "Kimbap",
-    image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
   }
-];
-
-function App() {
-  return (
-    <div className="App">
-      {foodILike.map(food_obj =>
-        <Food key={food_obj.id} name={food_obj.name} image={food_obj.image} />
-      )}
-    </div>
-  );
+  async componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isLoading: false })
+    }, 4000);
+    const movies = axios.get('https://yts.mx/api/v2/list_movies.json')
+  }
+  render() {
+    const { isLoading } = this.state;
+    return (
+      <div>{isLoading ? "Loading..." : "Ready"}</div>
+    );
+  }
 }
-
 export default App;
+
+// https://yts.mx/api/v2/list_movies.json
